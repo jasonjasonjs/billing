7e4d9db4c758f40e01d71bbdf0a3109097cf58d3
jasonjasonjs <jasontsai94@gmail.com>
2018-01-13 18:05:34 +0800

    1/13
    jason

---
+++ billing/JS/account.js
@@ -0,0 +1,23 @@
+var fdb = new ForerunnerDB();
+var db = fdb.db("account");
+var accountingCollection = db.collection('account');
+
+
+accountingCollection.load();
+
+
+function submit(){
+	var newAccount = {
+		date: $("#date").val(),
+		kind: $("#kind").val(),
+		item: $("#item").val(),
+		cost: $("#cost").val()
+	}
+	accountingCollection.insert(newAccount);
+	accountingCollection.save();
+	$("#date").val("");
+	$("#kind").val("");
+	$("#item").val("");
+	$("#cost").val("");
+	alert("儲存成功");
+}
